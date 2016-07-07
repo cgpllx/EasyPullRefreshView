d5fdaecd0be397fbb3258bd1a3f29468ffad756f
cgpllx <cgpllx1@qq.com>
2016-07-07 19:00:15 +0800

    Initial commit

---
+++ demo/src/main/java/com/liaoinstan/demospring/demo1/Demo1Activity.java
@@ -0,0 +1,59 @@
+package com.liaoinstan.demospring.demo1;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import com.liaoinstan.demospring.R;
+import com.liaoinstan.springview.container.DefaultFooter;
+import com.liaoinstan.springview.container.DefaultHeader;
+import com.liaoinstan.springview.widget.SpringView;
+
+public class Demo1Activity extends AppCompatActivity {
+    private SpringView springView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demo1);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        springView = (SpringView) findViewById(R.id.springview);
+        springView.setType(SpringView.Type.FOLLOW);
+        springView.setListener(new SpringView.OnFreshListener() {
+            @Override
+            public void onRefresh() {
+                System.out.println("SpringView" + "onRefresh");
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        springView.onFinishFreshAndLoad();
+
+                    }
+                }, 3000);
+            }
+
+            @Override
+            public void onLoadmore() {
+                System.out.println("SpringView" + "onLoadmore");
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        springView.onFinishFreshAndLoad();
+                    }
+                }, 3000);
+            }
+        });
+        springView.setHeader(new DefaultHeader(this));
+        springView.setFooter(new DefaultFooter(this));
+
+//        springView.postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//                springView.callFresh();
+//            }
+//        },200);
+    }
+}
---
+++ library/src/main/java/com/liaoinstan/springview/abs/PullViewHandle.java
@@ -0,0 +1,10 @@
+package com.liaoinstan.springview.abs;
+
+/**
+ * Created by cgpllx on 2016/7/7.
+ */
+public interface PullViewHandle {
+    boolean isLoadIng();
+
+    boolean isRefreshIng();
+}
